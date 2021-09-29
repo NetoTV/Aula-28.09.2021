ac93d28ffedad61cca3d3391844a588ec956b837
Paulo <pauloleandrolopezzz@gmail.com>
2021-09-28 22:20:04 -0300

    Aula 28.09.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from './clientes/cliente.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto';
+  clientes: Cliente[] = [];
+
+  onClienteAdicionado(cliente: Cliente) {
+    this.clientes.push(cliente);
+  }
+
 }
